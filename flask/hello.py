2256e83aaa26d4ceac4528656ab2ec24ef023c8f
Alex Cook <alexcook@Alexs-MBP-2.home>
2021-10-08 20:25:38 -0400

    first route

--- flask/hello.py
+++ flask/hello.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! test"
+
+@app.route("/route/")
+def route():
+    return "This worked"
 
 if __name__ == "__main__":
     app.run()
