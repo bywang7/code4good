f37bec0626932bd4b85f79e02a9c90979f2b28c2
Benjamin Wang <benwang@Benjamins-MacBook-Pro.local>
2021-10-09 00:29:44 -0500

    added chatt where you can type and send message

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
 
 @app.route("/")
 def index():
@@ -15,5 +18,16 @@ def signup():
 def profile():
     return render_template('profile.html')
 
+@app.route("/chat")
+def chat():
+    return render_template('chat.html')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
+
+
 if __name__ == "__main__":
     app.run()
+    #socketio.run(app)
